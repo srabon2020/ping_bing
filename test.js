69732129d76de0cb42f692b1a1c069adc886e3ac
docluv <chris@love2dev.com>
2019-08-07 17:53:15 -0400

    added basic tests

--- test.js
+++ test.js
@@ -1,4 +1,34 @@
-const pingBing = require( "index.js" );
+const pingBing = require( "index.js" ),
+    apiKey = "[Your API Key Here]",
+    siteUrl = "[Your Bing Verified Site URL (domain)]";
 
 
 //TODO: Write Tests :)
+
+const singleURL = {
+    apiKey: apiKey,
+    siteUrl: siteUrl,
+    url: "[url to submit]"
+};
+
+const batchURLs = {
+    apiKey: apiKey,
+    siteUrl: siteUrl,
+    urlList: [ "[url to submit]", "[url to submit]", "[url to submit]" ]
+};
+
+pingBing.pingBing( singleURL )
+    .then( body => {
+        console.log( "success: ", body );
+    } )
+    .catch( err => {
+        console.log( "error: ", err );
+    } );
+
+pingBing.pingBing( batchURLs )
+    .then( body => {
+        console.log( "success: ", body );
+    } )
+    .catch( err => {
+        console.log( "error: ", err );
+    } );
